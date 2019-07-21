72a03bab1237e112ac1e5d3262af6b71f7f094f0
Dmitry Ruznyaev <rewalkerof@gmail.com>
2019-07-21 20:41:33 +0300

    added template and inheritance

--- djblog/djblog/urls.py
+++ djblog/djblog/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib import admin
 
 admin.site.site_header = 'Тот еще секс'
 admin.site.site_title = 'Тот еще секс'
@@ -25,3 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls', namespace='posts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
