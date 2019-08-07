d68169c1d44fac6e55aeb9eeead73be8239111b1
Dmitry Ruznyaev <rewalkerof@gmail.com>
2019-08-07 11:34:47 +0300

    rewriting crispy form

--- djblog/posts/forms.py
+++ djblog/posts/forms.py
@@ -1,14 +1,21 @@
+from crispy_forms.helper import FormHelper
 from django import forms
 
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = 'Title'
+        self.fields['draft'].label = 'Draft?'
+        self.fields['content'].label = 'Content of post'
+
     class Meta:
         model = Post
         fields = [
             'title',
-            'image',
             'content',
+            'image',
             'draft',
         ]
